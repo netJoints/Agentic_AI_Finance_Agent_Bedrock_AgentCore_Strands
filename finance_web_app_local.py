c029233f56debea8fc5bebe096d5ffa548406a98
Shahzad Ali <52764993+netJoints@users.noreply.github.com>
2025-10-04 15:30:21 -0700

    Update and rename finance_web_app.py to finance_web_app_local.py

--- finance_web_app_local.py
+++ finance_web_app_local.py
@@ -3,6 +3,7 @@ Enterprise Financial AI Platform - Web Interface
 Protected by Britive Dynamic Privileged Access Management
 This code is not using the hosted AWS Bedrock AgentCore yet
 This code is working flawlessly using local laptop running the agents
+This version has no MCP server yet.
 """
 
 from flask import Flask, render_template_string, request, jsonify, send_from_directory
